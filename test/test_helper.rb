c497bbda1ad0acc5fc30b543c5eb1831327e2718
zhouyunao <yunaozhou@gmail.com>
2018-10-09 15:34:01 +0000

    Finish layout and routes(chapter 5 is over)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
+ENV['RAILS_ENV'] ||= 'test'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
