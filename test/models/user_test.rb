38a105595cae6ed4fd7b06aae4d274d5242f50c3
zhouyunao <yunaozhou@gmail.com>
2018-11-08 16:15:24 +0000

    Chapter 9 is over.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
   def setup
     @user = User.new(name: "Example User",email: "user@example.com",
                     password: "foobar", password_confirmation: "foobar")
@@ -65,6 +65,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
   end
-  
-end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
+end
