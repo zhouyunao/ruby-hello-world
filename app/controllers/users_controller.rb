23c0861c5064ccd93cf21673b4f043a69cd5330b
zhouyunao <yunaozhou@gmail.com>
2018-11-10 09:51:36 +0000

    update to 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,only:[:edit,:update]
   def new
     @user = User.new
   end
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
@@ -36,4 +38,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -31,4 +31,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_template 'users/show'
       assert is_logged_in?
     end
+  
 end
