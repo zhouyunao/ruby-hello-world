8866a51f93941e0bc38a4c03962576027028583e
zhouyunao <yunaozhou@gmail.com>
2018-11-14 17:00:59 +0000

    update to 10.3

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'jquery-rails'
 gem 'jquery-ui-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'faker', '1.7.3'
+gem 'will_paginate', '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,only:[:edit,:update]
   def new
     @user = User.new
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
